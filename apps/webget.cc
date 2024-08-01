5d110a4b5e4f896b5cc75d0111eb6f966a5359aa
Fangshuo Cao <alstoncao1999@gmail.com>
2024-07-31 21:38:19 -0700

    webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -1,4 +1,4 @@
-#include "tcp_sponge_socket.hh"
+#include "socket.hh"
 #include "util.hh"
 #include <cstdlib>
 #include <iostream>
@@ -11,7 +11,7 @@ void get_URL(const string &host, const string &path) {
     // Then you'll need to print out everything the server sends back,
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
-    CS144TCPSocket sock;
+    TCPSocket sock;
     sock.connect(Address(host, "http"));
     sock.write("GET " + path + " HTTP/1.1\r\n");
     sock.write("Host: " + host + "\r\n");
@@ -22,7 +22,6 @@ void get_URL(const string &host, const string &path) {
         cout << sock.read();
     }
     sock.close();
-    sock.wait_until_closed();
     cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
     cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
